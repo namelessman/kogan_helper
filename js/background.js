d50e08366f077f1a199124ce25633b11daa31ae5
namelessman <zhoumq.good@gmail.com>
2025-04-15 23:48:28 +1200

    Background query (#1)
    * feat: get data at any website; add notification;
    
    * fix: modify low data threshold;
    
    * feat: disable notification when not login
    
    * fix: change cache when logged out

---
+++ js/background.js
@@ -0,0 +1,157 @@
+import {
+  LOW_DATA_THRESHOLD,
+  NOTIFICATION_ICON_URL,
+  NOTIFICATION_TITLE,
+  CHECK_INTERVAL,
+  IS_MOCK,
+} from "./config.js";
+import { mock } from "./mockData.js";
+
+let myNotificationID = null;
+
+function isLowData(remaining, total) {
+  return (remaining / total) * 100 < LOW_DATA_THRESHOLD;
+}
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.local.get(["enabledNotify"], (result) => {
+    if (result.enabledNotify) {
+      chrome.alarms.create("dailyCheck", { periodInMinutes: CHECK_INTERVAL });
+    }
+  });
+});
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+  chrome.storage.local.get(["enabledNotify"], (result) => {
+    if (!result.enabledNotify) return;
+    if (alarm.name === "dailyCheck") dailyCheck();
+  });
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "getUsageSummary") fetchUsageSummary();
+  if (message.action === "testNotification") dailyCheck(true);
+});
+
+chrome.notifications.onButtonClicked.addListener((notifId, btnIdx) => {
+  if (notifId === myNotificationID && btnIdx === 0) {
+    chrome.storage.local.set({ enabledNotify: false }, () => {
+      console.log("Notification disabled");
+    });
+  }
+});
+
+async function dailyCheck(forceNotification = false) {
+  const result = await chrome.storage.local.get(["enabledNotify"]);
+  if (!result.enabledNotify) return;
+
+  if (IS_MOCK) {
+    const remaining = +mock.plan.entitlements[0].remainingAllowance.amount;
+    const total = +mock.plan.entitlements[0].totalAllowance.amount;
+    if (forceNotification || isLowData(remaining, total)) {
+      chrome.notifications.create(
+        {
+          type: "basic",
+          buttons: [{ title: "Don't show again" }],
+          iconUrl: NOTIFICATION_ICON_URL,
+          title: NOTIFICATION_TITLE,
+          message: `You have ${remaining} ${mock.plan.entitlements[0].remainingAllowance.unit} data remaining.`,
+        },
+        (id) => {
+          myNotificationID = id;
+        }
+      );
+    }
+  } else {
+    const result = await chrome.storage.local.get(["accountId"]);
+    const accountId = result.accountId;
+    console.log("Account ID currently is " + accountId);
+
+    if (!accountId) return;
+    fetch(
+      `https://accounts.koganmobile.co.nz/api/account/usage_summary/${accountId}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (!data?.plan) {
+          chrome.storage.local.set(
+            { enabledNotify: false }
+            //   () => {
+            //   chrome.notifications.create({
+            //     type: "basic",
+            //     iconUrl: NOTIFICATION_ICON_URL,
+            //     title: NOTIFICATION_TITLE,
+            //     message:
+            //       "Please log in to Kogan Mobile to check your data usage daily.",
+            //   });
+            // }
+          );
+          return;
+        }
+        const remaining =
+          +data?.plan?.entitlements?.[0]?.remainingAllowance.amount;
+        const total = +data?.plan?.entitlements?.[0]?.totalAllowance?.amount;
+        if (forceNotification || isLowData(remaining, total)) {
+          chrome.notifications.create(
+            {
+              type: "basic",
+              buttons: [{ title: "Don't show again" }],
+              iconUrl: NOTIFICATION_ICON_URL,
+              title: NOTIFICATION_TITLE,
+              message: `You have ${remaining} ${data?.plan?.entitlements?.[0]?.remainingAllowance.unit} data remaining.`,
+            },
+            (id) => {
+              myNotificationID = id;
+            }
+          );
+        }
+      })
+      .catch((error) => console.error("Error fetching usage summary:", error));
+  }
+}
+
+function setAccountId(accountId) {
+  chrome.storage.local.set({ accountId }, () => {
+    console.log("Account ID is set to " + accountId);
+  });
+}
+
+function init() {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const tab = tabs[0];
+    if (
+      tab &&
+      tab.url?.startsWith("https://accounts.koganmobile.co.nz/services/")
+    ) {
+      const accountId = tab.url.split("/services/")[1]?.split("/")?.[0];
+      setAccountId(accountId);
+    }
+  });
+}
+
+async function fetchUsageSummary() {
+  const result = await chrome.storage.local.get(["accountId"]);
+  const accountId = result.accountId;
+  console.log("Account ID currently is " + accountId);
+
+  if (!accountId) return;
+
+  if (IS_MOCK) {
+    chrome.runtime.sendMessage({ action: "usageSummaryData", data: mock });
+    return;
+  }
+
+  fetch(
+    `https://accounts.koganmobile.co.nz/api/account/usage_summary/${accountId}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      if (!data?.plan) {
+        return;
+      }
+      chrome.runtime.sendMessage({ action: "usageSummaryData", data: data });
+    })
+    .catch((error) => console.error("Error fetching usage summary:", error));
+}
+
+init();
--- popup.js
+++ popup.js
@@ -1,39 +1,98 @@
 chrome.runtime.sendMessage({ action: "getUsageSummary" });
 
-
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   if (message.action === "usageSummaryData") {
     displayUsageSummary(message.data);
+
+    setTimeout(() => {
+      document
+        .getElementById("enable_notify")
+        .addEventListener("change", onNotifyChange);
+      document
+        .getElementById("testNotification")
+        .addEventListener("click", function () {
+          chrome.runtime.sendMessage({ action: "testNotification" });
+        });
+    }, 100);
   }
 });
 
 function calculateDaysBetween(date1, date2) {
   const d1 = new Date(date1);
   const d2 = new Date(date2);
-  
+
   const utc1 = Date.UTC(d1.getFullYear(), d1.getMonth(), d1.getDate());
   const utc2 = Date.UTC(d2.getFullYear(), d2.getMonth(), d2.getDate());
-  
+
   return Math.ceil((utc2 - utc1) / (1000 * 60 * 60 * 24));
+}
+
+function onNotifyChange() {
+  if (document.getElementById("enable_notify").checked) {
+    chrome.storage.local.set({ enabledNotify: true }, function () {
+      console.log("Notification enabled");
+    });
+
+    document.getElementById("testNotification").style.display = "block";
+  } else {
+    chrome.storage.local.set({ enabledNotify: false }, function () {
+      console.log("Notification disabled");
+    });
+
+    document.getElementById("testNotification").style.display = "none";
+  }
 }
 
 function displayUsageSummary(data) {
   const usageSummaryDiv = document.getElementById("usageSummary");
 
-  const plan = data.plan;
-  const dataEntitlement = plan.entitlements.find(entitlement => entitlement.entitlementType === "DATA");
+  const plan = data?.plan;
+  if (!plan) {
+    return;
+  }
+  const dataEntitlement = plan.entitlements.find(
+    (entitlement) => entitlement.entitlementType === "DATA"
+  );
   const today = new Date();
-  const todayString = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+  const todayString =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
 
-  usageSummaryDiv.innerHTML = `
-    <div class="section">
-      <p><strong>Plan:</strong> ${plan.title}</p>
-      <p><strong>Description:</strong> ${plan.description}</p>
-      <p><strong>Data Allowance:</strong> ${dataEntitlement.totalAllowance.amount} ${dataEntitlement.totalAllowance.unit}</p>
-      <p><strong>Remaining Data:</strong> ${dataEntitlement.remainingAllowance.amount} ${dataEntitlement.remainingAllowance.unit}</p>
-      <p class="highlight mb-0"><strong>End Date:</strong> ${plan.end_date_inclusive}</p>
-      <p class="mb-0"><strong>Today:</strong> ${todayString}</p>
-      <p><strong>Days Remaining:</strong> ${calculateDaysBetween(todayString, plan.end_date_inclusive)}</p>
+  chrome.storage.local.get("enabledNotify", function (result) {
+    console.log("enabledNotify", result.enabledNotify);
+    const innerHTML = `
+        <div>
+      <div class="section">
+        <p><strong>Plan:</strong> ${plan.title}</p>
+        <p><strong>Description:</strong> ${plan.description}</p>
+        <p><strong>Data Allowance:</strong> ${
+          dataEntitlement.totalAllowance.amount
+        } ${dataEntitlement.totalAllowance.unit}</p>
+        <p><strong>Remaining Data:</strong> ${
+          dataEntitlement.remainingAllowance.amount
+        } ${dataEntitlement.remainingAllowance.unit}</p>
+        <p class="highlight mb-0"><strong>End Date:</strong> ${
+          plan.end_date_inclusive
+        }</p>
+        <p class="mb-0"><strong>Today:</strong> ${todayString}</p>
+        <p><strong>Days Remaining:</strong> ${calculateDaysBetween(
+          todayString,
+          plan.end_date_inclusive
+        )}</p>
+      </div>
+      <div class="ope_section">
+      <input type="checkbox" ${
+        result.enabledNotify ? "checked" : ""
+      } name="enable_notify" id="enable_notify"/>
+      <label for="enable_notify" class="tooltip">
+      Alert me on low data
+        <span class="tooltiptext">Checks daily and alerts if data drops below 30%</span>
+      </label>
+      <button id="testNotification" style="display: ${
+        result.enabledNotify ? "block" : "none"
+      }">Test Notification</button>
+      </div>
     </div>
-  `;
+    `;
+    usageSummaryDiv.innerHTML = innerHTML;
+  });
 }
